f817efd1f742b79d33e6dbc477f54709b397bf38
Bruno Borges Fonseca <b.f_04@hotmail.com>
2020-01-27 16:12:19 -0300

    Fix moa.bat script

--- moa/src/main/scripts/moa.bat
+++ moa/src/main/scripts/moa.bat
@@ -3,5 +3,4 @@
 set BASEDIR=%~dp0\..
 set MEMORY=512m
 
-java -Xmx%MEMORY% -cp "%BASEDIR%/lib/*" -javaagent:"%BASEDIR%/lib/sizeofag-1.0.4.jar" moa.gui.GUI
-
+java -Xmx%MEMORY% -cp "%BASEDIR%/lib/moa-2018.6.0;%BASEDIR%/lib/*" -javaagent:"%BASEDIR%/lib/sizeofag-1.0.4.jar" moa.gui.GUI
